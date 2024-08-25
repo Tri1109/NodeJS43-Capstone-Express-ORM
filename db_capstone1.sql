2783d964a420b655e73242c19dd23ee3c0204036
Tri1109 <157017449+Tri1109@users.noreply.github.com>
2024-08-25 20:03:14 +0700

    db_capstone1
    Database file

---
+++ db_capstone1.sql
@@ -0,0 +1,106 @@
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+CREATE TABLE `binh_luan` (
+  `binh_luan_id` int NOT NULL AUTO_INCREMENT,
+  `nguoi_dung_id` int DEFAULT NULL,
+  `hinh_id` int DEFAULT NULL,
+  `ngay_binh_luan` date NOT NULL,
+  `noi_dung` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`binh_luan_id`),
+  KEY `nguoi_dung_id` (`nguoi_dung_id`),
+  KEY `hinh_id` (`hinh_id`),
+  CONSTRAINT `binh_luan_ibfk_1` FOREIGN KEY (`nguoi_dung_id`) REFERENCES `nguoi_dung` (`nguoi_dung_id`) ON DELETE CASCADE,
+  CONSTRAINT `binh_luan_ibfk_2` FOREIGN KEY (`hinh_id`) REFERENCES `hinh_anh` (`hinh_id`) ON DELETE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+CREATE TABLE `hinh_anh` (
+  `hinh_id` int NOT NULL AUTO_INCREMENT,
+  `ten_hinh` varchar(255) NOT NULL,
+  `duong_dan` varchar(255) NOT NULL,
+  `mo_ta` varchar(255) DEFAULT NULL,
+  `nguoi_dung_id` int DEFAULT NULL,
+  PRIMARY KEY (`hinh_id`),
+  KEY `nguoi_dung_id` (`nguoi_dung_id`),
+  CONSTRAINT `hinh_anh_ibfk_1` FOREIGN KEY (`nguoi_dung_id`) REFERENCES `nguoi_dung` (`nguoi_dung_id`) ON DELETE SET NULL
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+CREATE TABLE `luu_anh` (
+  `nguoi_dung_id` int NOT NULL,
+  `hinh_id` int NOT NULL,
+  `ngay_luu` date NOT NULL,
+  PRIMARY KEY (`nguoi_dung_id`,`hinh_id`),
+  KEY `hinh_id` (`hinh_id`),
+  CONSTRAINT `luu_anh_ibfk_1` FOREIGN KEY (`nguoi_dung_id`) REFERENCES `nguoi_dung` (`nguoi_dung_id`) ON DELETE CASCADE,
+  CONSTRAINT `luu_anh_ibfk_2` FOREIGN KEY (`hinh_id`) REFERENCES `hinh_anh` (`hinh_id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+CREATE TABLE `nguoi_dung` (
+  `nguoi_dung_id` int NOT NULL AUTO_INCREMENT,
+  `email` varchar(255) NOT NULL,
+  `mat_khau` varchar(255) NOT NULL,
+  `ho_ten` varchar(255) NOT NULL,
+  `tuoi` int DEFAULT NULL,
+  `anh_dai_dien` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`nguoi_dung_id`),
+  UNIQUE KEY `email` (`email`) USING BTREE
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+INSERT INTO `binh_luan` (`binh_luan_id`, `nguoi_dung_id`, `hinh_id`, `ngay_binh_luan`, `noi_dung`) VALUES
+(5, 1, 5, '2024-08-01', 'Hình ảnh rất đẹp!');
+INSERT INTO `binh_luan` (`binh_luan_id`, `nguoi_dung_id`, `hinh_id`, `ngay_binh_luan`, `noi_dung`) VALUES
+(6, 2, 6, '2024-08-02', 'Tôi rất thích bức ảnh này.');
+INSERT INTO `binh_luan` (`binh_luan_id`, `nguoi_dung_id`, `hinh_id`, `ngay_binh_luan`, `noi_dung`) VALUES
+(7, 2, 7, '2024-08-03', 'Cảm ơn vì đã chia sẻ hình ảnh này.');
+INSERT INTO `binh_luan` (`binh_luan_id`, `nguoi_dung_id`, `hinh_id`, `ngay_binh_luan`, `noi_dung`) VALUES
+(8, 1, 8, '2024-08-04', 'Bức ảnh này thật sự tuyệt vời.'),
+(9, 1, 5, '2024-08-01', 'Hình ảnh rất đẹp!'),
+(10, 2, 6, '2024-08-02', 'Tôi rất thích bức ảnh này.'),
+(11, 2, 7, '2024-08-03', 'Cảm ơn vì đã chia sẻ hình ảnh này.'),
+(12, 1, 8, '2024-08-04', 'Bức ảnh này thật sự tuyệt vời.'),
+(14, 2, 9, '2024-08-25', 'Test bình luận!'),
+(15, 2, 9, '2024-08-25', 'Test bình luận 234!'),
+(16, 2, 9, '2024-08-25', 'lưu bình luận!'),
+(17, 2, 6, '2024-08-25', 'ảnh đẹp!'),
+(18, 3, 6, '2024-08-25', 'ok good!');
+
+INSERT INTO `hinh_anh` (`hinh_id`, `ten_hinh`, `duong_dan`, `mo_ta`, `nguoi_dung_id`) VALUES
+(5, 'hinh_1', '/images/hinh1.jpg', 'Hình ảnh 1', 1);
+INSERT INTO `hinh_anh` (`hinh_id`, `ten_hinh`, `duong_dan`, `mo_ta`, `nguoi_dung_id`) VALUES
+(6, 'hinh_2', '/images/hinh2.jpg', 'Hình ảnh 2', 2);
+INSERT INTO `hinh_anh` (`hinh_id`, `ten_hinh`, `duong_dan`, `mo_ta`, `nguoi_dung_id`) VALUES
+(7, 'hinh_3', '/images/hinh3.jpg', 'Hình ảnh 3', 1);
+INSERT INTO `hinh_anh` (`hinh_id`, `ten_hinh`, `duong_dan`, `mo_ta`, `nguoi_dung_id`) VALUES
+(8, 'hinh_4', '/images/hinh4.jpg', 'Hình ảnh 4', 2),
+(9, 'hinh_13', '/images/hinh13.jpg', 'Hình ảnh 13', 1),
+(11, 'Venom', '/images/venom.jpg', 'Đây là hình ảnh venom.', 2),
+(13, 'batman', '/images/batman.jpg', 'Đây là hình ảnh batman.', 2);
+
+INSERT INTO `luu_anh` (`nguoi_dung_id`, `hinh_id`, `ngay_luu`) VALUES
+(2, 9, '2024-08-25');
+INSERT INTO `luu_anh` (`nguoi_dung_id`, `hinh_id`, `ngay_luu`) VALUES
+(2, 11, '2024-08-25');
+INSERT INTO `luu_anh` (`nguoi_dung_id`, `hinh_id`, `ngay_luu`) VALUES
+(2, 13, '2024-08-25');
+
+INSERT INTO `nguoi_dung` (`nguoi_dung_id`, `email`, `mat_khau`, `ho_ten`, `tuoi`, `anh_dai_dien`) VALUES
+(1, 'thothanhtri1109@gmail.com', '$2b$10$4U.JlXXKGpyNJHt1DIlgy.JiHj7AdXSm3Y91A2O/bkaH0pW0nckiC', 'Trí', 28, '');
+INSERT INTO `nguoi_dung` (`nguoi_dung_id`, `email`, `mat_khau`, `ho_ten`, `tuoi`, `anh_dai_dien`) VALUES
+(2, 'thothanhtri1996@gmail.com', '$2b$10$W/4vhmD7KtDlAfVZe5LeoOJLjSX4mUkErZ3FrhwLS.lNJO8etf3Au', 'Trí', 28, '/images/tri.jpg');
+INSERT INTO `nguoi_dung` (`nguoi_dung_id`, `email`, `mat_khau`, `ho_ten`, `tuoi`, `anh_dai_dien`) VALUES
+(3, 'thothanhtri0812@gmail.com', '$2b$10$x6BSTtq5/hqqX4EqId7IyOQdAWeN2S4z5JgMcccks/5pzlNCYLYNG', 'Tèo', 30, '');
+
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
